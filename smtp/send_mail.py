7d129af39e3714abf1184058e0c1d9c15cedf7eb
胡一帆 <yifan.hu@geely.com>
2020-01-03 16:39:07 +0800

    xxxx

--- smtp/send_mail.py
+++ smtp/send_mail.py
@@ -1,23 +1,42 @@
-#!/usr/bin/python3
-
 import smtplib
 from email.mime.text import MIMEText
-from email.header import Header
+# 引入smtplib和MIMEText
+from time import sleep
 
-sender = 'nani@localhost'
-receivers = ['yifan.hu@geely.com']  # 接收邮件，可设置为你的QQ邮箱或者其他邮箱
 
-# 三个参数：第一个为文本内容，第二个 plain 设置文本格式，第三个 utf-8 设置编码
-message = MIMEText('Python 邮件发送测试...', 'plain', 'utf-8')
-message['From'] = Header("菜鸟教程", 'utf-8')  # 发送者
-message['To'] = Header("测试", 'utf-8')  # 接收者
+def sentemail():
+    host = 'smtp.163.com'
+    # 设置发件服务器地址
+    port = 465
+    # 设置发件服务器端口号。注意，这里有SSL和非SSL两种形式，现在一般是SSL方式
+    sender = 'huyifan_zju@163.com'
+    # 设置发件邮箱，一定要自己注册的邮箱
+    pwd = 'xxxxxx'
+    # 设置发件邮箱的授权码密码，根据163邮箱提示，登录第三方邮件客户端需要授权码
+    receiver = 'demotto@zju.edu.cn'
+    # 设置邮件接收人，可以是QQ邮箱
+    body = '<h1>你已成功打卡</h1><p>zhongfs</p>'
+    # 设置邮件正文，这里是支持HTML的
+    msg = MIMEText(body, 'html')
+    # 设置正文为符合邮件格式的HTML内容
+    msg['subject'] = '打卡通知'
+    # 设置邮件标题
+    msg['from'] = sender
+    # 设置发送人
+    msg['to'] = receiver
+    # 设置接收人
+    try:
+        #s = smtplib.SMTP_SSL(host, port)
+        s = smtplib.SMTP(host)
+        # 注意！如果是使用SSL端口，这里就要改为SMTP_SSL
+        s.login(sender, pwd)
+        # 登陆邮箱
+        s.sendmail(sender, receiver, msg.as_string())
+        # 发送邮件！
+        print('Done.sent email success')
+    except smtplib.SMTPException:
+        print('Error.sent email fail')
 
-subject = 'Python SMTP 邮件测试'
-message['Subject'] = Header(subject, 'utf-8')
 
-try:
-    smtpObj = smtplib.SMTP(host='localhost', port=10025)
-    smtpObj.sendmail("xxx@xxx.com", receivers, message.as_string())
-    print("邮件发送成功")
-except smtplib.SMTPException:
-    print("Error: 无法发送邮件")
+if __name__ == '__main__':
+    sentemail()

6719526bd2b2fee4964f020bed1da28518c55b12
胡一帆 <yifan.hu@geely.com>
2020-01-06 15:57:44 +0800

    aaaaa

--- alertmanager/start.sh
+++ alertmanager/start.sh
@@ -1,3 +1,5 @@
 BASE_DIR=$(cd `dirname $0`/..; pwd)
 CONFIG=${BASE_DIR}/config/alertmanager.yml
-docker run -d -p 9093:9093 -v ${CONFIG}:/etc/alertmanager/alertmanager.yml --name alertmanager docker.io/prom/alertmanager:latest
+cmd="docker run -d -p 9093:9093 -v ${CONFIG}:/etc/alertmanager/alertmanager.yml --name alertmanager docker.io/prom/alertmanager:latest"
+echo $cmd
+$cmd

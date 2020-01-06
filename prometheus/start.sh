f57ff7476e1beae9f1054e180a81d49a6e48477a
胡一帆 <yifan.hu@geely.com>
2020-01-06 15:21:05 +0800

    aaaaa

--- prometheus/start.sh
+++ prometheus/start.sh
@@ -2,5 +2,6 @@ BASE_DIR=$(cd `dirname $0`/..; pwd)
 CONFIG=${BASE_DIR}/config/prometheus.yml
 ALERT_RULES=${BASE_DIR}/config/alertmanager_rules.yml
 
-cmd="docker run -d -p 9090:9090 -v ${CONFIG}:/etc/prometheus/prometheus.yml ${ALERT_RULES}:/etc/prometheus/alertmanager_rules.yml prom/prometheus:latest"
+cmd="docker run -d -p 9090:9090 -v ${CONFIG}:/etc/prometheus/prometheus.yml -v ${ALERT_RULES}:/etc/prometheus/alertmanager_rules.yml prom/prometheus:latest"
 echo $cmd
+$cmd
